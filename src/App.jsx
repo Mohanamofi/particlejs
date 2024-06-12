a974aac707949c9145664e0453d3452f8f215ffa
Mohanamofi <mohanasundari7067@gmail.com>
2024-06-12 10:31:13 -0700

    File created

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+
+import './App.css'
+import Loginpage from './Loginpage'
+import ParticlesComponent from './Particles'
+
+function App() {
+
+
+  return (
+    <>
+     <Loginpage />
+     <ParticlesComponent />
+    </>
+  )
+}
+
+export default App
---
+++ src/Loginpage.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Loginpage = () => {
+  return (
+    <div className="container">
+      <h1>As Global soft tech</h1>
+      <input type="text" placeholder="Username" />
+      <input type="password" placeholder="Password" />
+      <p><a href="#">Forget Password !!</a></p>
+      <button>Login</button>
+    </div>
+  )
+}
+
+export default Loginpage
